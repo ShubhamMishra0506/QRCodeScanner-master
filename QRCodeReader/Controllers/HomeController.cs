e6808e40daad95681e7c86b4329d6ef86b86f9d6
ShubhamMishra0506 <70952168+ShubhamMishra0506@users.noreply.github.com>
2021-08-23 10:22:14 +0530

    Update HomeController.cs
    To Test Webhook

--- QRCodeReader/Controllers/HomeController.cs
+++ QRCodeReader/Controllers/HomeController.cs
@@ -11,9 +11,11 @@ namespace QRCodeReader.Controllers
 {
     public class HomeController : Controller
     {
+    
         public ActionResult Index()
         {
             return View();
+            
         }
         [HttpPost]
         public ActionResult UploadQRCodeImage()
